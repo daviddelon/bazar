9142d9dfbabf58145156d759e850c7054c1a8930
Florian Schmitt <florian@outils-reseaux.org>
2012-06-13 18:48:42 +0200

    correction bug méthode bazarframe

--- actions/linkjavascript__.php
+++ actions/linkjavascript__.php
@@ -5,7 +5,7 @@ if (!defined("WIKINI_VERSION"))
             die ("acc&egrave;s direct interdit");
 }
 
-if ($this->GetMethod() == "show" || $this->GetMethod() == "/bazarframe" || $this->GetMethod() == "edit") {
+if ($this->GetMethod() == "show" || $this->GetMethod() == "bazarframe" || $this->GetMethod() == "edit") {
 	echo '	<script src="tools/bazar/libs/bazar.js"></script>'."\n";	
 }
 
